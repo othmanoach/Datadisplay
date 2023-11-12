e20ecd5d69b37b0b34880d0e7492d9b2efbf1200
hepona <hepona10@gmail.com>
2023-11-12 22:42:19 +0100

    wip show data on browser

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
-from flask import Flask, render_template, request, redirect, url_for
-import csv
+# importing flask
+from flask import Flask, render_template, request, url_for, redirect
+import pandas as pd
 import os
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "uploaded_csvFiles"
+#####################from doc########################
+ALLOWED_EXT = set(["csv"])
 
-if not os.path.exists(app.config["UPLOAD_FOLDER"]):
-    os.makedirs(app.config["UPLOAD_FOLDER"])
-
-
-def allowed_file(file_name):
-    """vérifier l'extension du fichier"""
-    return "." in file_name and file_name.rsplit(".", 1)[1].lower() == "csv"
+def allowed_file(filename):
+    """check if uploaded file has the right extension"""
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXT
 
 
+#############################################
+
+app = Flask(__name__, static_url_path='/static')
+
+
+# route to html page - "table"
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+###################################################################
+
+
 @app.route("/", methods=["POST"])
-def upload_file():
-    print("test")
+def upload():
     if request.method == "POST":
-        filename = request.files["filename"]
-        if allowed_file(filename):
-            df = pd.read_csv(filename)
-            print(df.head())
-            return render_template("index.html")
+        f = request.files["file"]
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join("uploaded_File", filename))
 
-    else:
-        return render_template("index.html")
+            return redirect(url_for("table", filename=filename))
+
+    return render_template("index.html")
 
 
-# route pour afficher la table des données du fichier importé
-@app.route("/table/<string:file_name>")
-def table(file_name):
-    return render_template("table.html", file_name=file_name)
+@app.route("/table/<filename>")
+def table(filename):
+    upload_folder = os.path.join(os.getcwd(), "uploaded_File")
+    file_path = os.path.join(upload_folder, filename)
+    data = pd.read_csv(file_path, sep='[;,\t|:@]', engine='python')
+    return render_template("table.html", tables=[data.to_html()], titles=[""])
 
 
 if __name__ == "__main__":
