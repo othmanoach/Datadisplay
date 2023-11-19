cb30415d6300034fac055ce23c735fb4b28aa868
hepona <hepona10@gmail.com>
2023-11-19 22:59:57 +0100

    Line chart: added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import os
 st.title("Import your .CSV file here :")
 st.markdown("---")
 file = st.file_uploader("**Choose a .csv file**", type={"csv"})
@@ -14,8 +14,13 @@ if file is not None:
             "Number of line", min_value=1, max_value=len(df), value=val_head
         )
         head = df.head(num_line)
+        #------------------------------------------
+        st.header("Line Chart")
+        x = st.selectbox("Select X value", df.columns)
+        y = st.selectbox("Select Y value", df.columns)
+        st.line_chart(head, x=x, y=y)
 
-        
 
     with col1:
+        st.header("Dataframe")
         st.dataframe(head, hide_index=True)
