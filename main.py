a9a5f56ca92803f2c6deea6ca228b41bf0b24897
hepona <hepona10@gmail.com>
2023-11-26 18:17:51 +0100

    add more chart

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import streamlit as st
 import pandas as pd
-import os
-st.title("Import your .CSV file here :")
+import plotly.express as px
+
+st.title("Import your .CSV file here:")
 st.markdown("---")
-file = st.file_uploader("**Choose a .csv file**", type={"csv"})
+file = st.file_uploader("Choose a .csv file", type=["csv"])
 
 if file is not None:
     col1, col2 = st.columns(2)
-    df = pd.read_csv(file, sep="[;,\t|:@]",  engine='python')
+    try:
+        df = pd.read_csv(file, sep="[;,\t|:@]", engine='python')
+    except Exception as e:
+        st.error(f"Error: {e}")
+
     with col2:
         val_head = 5
-        num_line = st.slider(
-            "Number of line", min_value=1, max_value=len(df), value=val_head
-        )
+        num_line = st.slider("Number of lines", min_value=1, max_value=len(df), value=val_head)
         head = df.head(num_line)
-        #------------------------------------------
-        st.header("Line Chart")
+
         x = st.selectbox("Select X value", df.columns)
         y = st.selectbox("Select Y value", df.columns)
-        st.line_chart(head, x=x, y=y)
 
+        type_chart = st.selectbox("Select chart type", ['Simple bar charts', 'Simple area charts', 'Pie'])
+        st.header(type_chart)
+
+        if type_chart == 'Simple bar charts':
+            st.bar_chart(head, x=x, y=y)
+        elif type_chart == 'Simple area charts':
+            st.area_chart(head, x=x, y=y)
+        elif type_chart == 'Pie':
+            fig = px.pie(head, names=x, values=y)
+            st.plotly_chart(fig)
 
     with col1:
         st.header("Dataframe")
-        st.dataframe(head, hide_index=True)
+        st.dataframe(head.head(), hide_index=True)
